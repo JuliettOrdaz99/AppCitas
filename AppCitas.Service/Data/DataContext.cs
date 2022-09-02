857d703f705614018fd7dd57390d3ff494a727e5
Arturo Barajas <basaar.24@gmail.com>
2022-09-02 07:51:52 -0500

    Adding a DbContext class

---
+++ AppCitas.Service/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using AppCitas.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
--- AppCitas.Service/Startup.cs
+++ AppCitas.Service/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 namespace AppCitas;
@@ -14,7 +15,10 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
+        services.AddDbContext<DbContext>(options =>
+        {
+            options.UseSqlite("Connection string");
+        });
         services.AddControllers();
         services.AddSwaggerGen(c =>
         {
