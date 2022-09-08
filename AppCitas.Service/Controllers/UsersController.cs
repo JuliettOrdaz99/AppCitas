d531e38c4beffda978f860ebcb633ada04eb1ff3
Arturo Barajas <basaar.24@gmail.com>
2022-09-08 07:23:45 -0500

    Adding a new API Controller

---
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCitas.Service.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _context.Users.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUserById(int id)
+    {
+        return _context.Users.Find(id);
+    }
+}
