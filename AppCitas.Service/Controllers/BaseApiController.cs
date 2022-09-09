5137207fe5bd2ed6a784d14ea77d6a4315f6f4ac
Arturo Barajas <basaar.24@gmail.com>
2022-09-09 07:33:55 -0500

    Creating a base API controller

---
+++ AppCitas.Service/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppCitas.Service.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+}
--- AppCitas.Service/Controllers/UsersController.cs
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -6,9 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AppCitas.Service.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class UsersController : ControllerBase
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
 
