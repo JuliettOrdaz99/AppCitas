ac7c10b6206c0fbfcdb701f4f1ae544f2810ce91
Arturo Barajas <basaar.24@gmail.com>
2022-09-09 07:50:33 -0500

    Creating an Account Controller with a register endpoint

---
+++ AppCitas.Service/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppCitas.Service.Controllers;
+
+public class AccountController : BaseApiController
+{
+	private readonly DataContext _context;
+
+	public AccountController(DataContext context)
+	{
+		_context = context;
+	}
+
+	[HttpPost("register")]
+	public async Task<ActionResult<AppUser>> Register(string username, string password)
+	{
+		using var hmac = new HMACSHA512();
+
+		var user = new AppUser
+		{
+			UserName = username,
+			PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+			PasswordSalt = hmac.Key
+		};
+
+		_context.Users.Add(user);
+		await _context.SaveChangesAsync();
+
+		return user;
+	}
+}
