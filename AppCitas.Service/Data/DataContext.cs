df7e6d80d641a7f6cd4b43e4c9f7f315f32b9198
JuliettOrdaz99 <ajov_189420@hotmail.com>
2022-09-11 16:48:27 -0500

    Creating the connection String

---
+++ AppCitas.Service/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AppCitas.Service.Entities;
+
+namespace AppCitas.Service.Data;
+
+public class DataContext : DbContext
+{
+   public DataContext(DbContextOptions options) : base(options)
+   {
+
+   }
+    public DbSet<AppUser> Users { get; set;}
+}
