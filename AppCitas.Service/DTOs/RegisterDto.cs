9256e4bd0cf04df097442278edd496c7321baab0
Arturo Barajas <basaar.24@gmail.com>
2022-09-13 07:16:59 -0500

    Adding validation

--- AppCitas.Service/DTOs/RegisterDto.cs
+++ AppCitas.Service/DTOs/RegisterDto.cs
@@ -1,7 +1,12 @@
-﻿namespace AppCitas.Service.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppCitas.Service.DTOs;
 
 public class RegisterDto
 {
+    [Required]
     public string Username { get; set; }
+    
+    [Required]
     public string Password { get; set; }
 }
