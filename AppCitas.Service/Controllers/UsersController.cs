dcf52db73f10a35af4078c97974caf8cdc00ee1f
JuliettOrdaz99 <ajov_189420@hotmail.com>
2022-09-17 14:53:17 -0500

    Making our code Asynchronous

---
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Controllers;
+
+public class UsersController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
