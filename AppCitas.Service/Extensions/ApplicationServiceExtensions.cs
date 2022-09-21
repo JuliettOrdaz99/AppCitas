789d9fed1e153eb2a44458df515a18bd3ebf8dce
Arturo Barajas <basaar.24@gmail.com>
2022-09-21 07:26:32 -0500

    Adding extension methods

---
+++ AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AppCitas.Service.Data;
+using AppCitas.Service.Interfaces;
+using AppCitas.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(config.GetConnectionString("DefaultConnection")
+            );
+        });
+
+        return services;
+    }
+}
