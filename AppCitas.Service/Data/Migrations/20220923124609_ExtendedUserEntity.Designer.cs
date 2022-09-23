7c6f0fac0257f02b73ce1f713ecf8f1beb6e777f
Arturo Barajas <basaar.24@gmail.com>
2022-09-23 07:50:05 -0500

    Entity Framework relationships

--- AppCitas.Service/Data/Migrations/20220923124609_ExtendedUserEntity.Designer.cs
+++ AppCitas.Service/Data/Migrations/20220923124609_ExtendedUserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AppCitas.Service.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppCitas.Service.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220923124609_ExtendedUserEntity")]
+    partial class ExtendedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
--- AppCitas.Service/Entities/Photo.cs
+++ AppCitas.Service/Entities/Photo.cs
@@ -1,9 +1,14 @@
-﻿namespace AppCitas.Service.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace AppCitas.Service.Entities;
+
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
+    public AppUser AppUser { get; set; }
+    public int AppUserId { get; set; }
 }
