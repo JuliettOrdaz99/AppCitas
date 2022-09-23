df598454d6b3ef0b7851c092552b3007fe0f2598
Arturo Barajas <basaar.24@gmail.com>
2022-09-23 07:21:56 -0500

    Extending the user entity

--- AppCitas.Service/Entities/AppUser.cs
+++ AppCitas.Service/Entities/AppUser.cs
@@ -6,4 +6,15 @@ public class AppUser
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public ICollection<Photo> Photos { get; set; }
 }
