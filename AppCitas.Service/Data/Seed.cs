b39c0908c9ffe39495e9fb277eea7a3b3e1a3e39
Arturo Barajas <basaar.24@gmail.com>
2022-09-26 07:34:14 -0500

    Seeding data part one

---
+++ AppCitas.Service/Data/Seed.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using AppCitas.Service.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCitas.Service.Data;
+
+public class Seed
+{
+    public static async Task SeedUsers(DataContext context)
+    {
+        if (await context.Users.AnyAsync()) return;
+
+        var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+            user.PasswordSalt = hmac.Key;
+
+            context.Users.Add(user);
+        }
+    }
+}
