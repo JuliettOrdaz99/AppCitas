5421f95928f0b119f18e397dad416b317814bc9e
Arturo Barajas <basaar.24@gmail.com>
2022-09-27 07:48:46 -0500

    Creating a repository

--- AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
+++ AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
@@ -10,6 +10,7 @@ public static class ApplicationServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
         services.AddDbContext<DataContext>(options =>
         {
             options.UseSqlite(config.GetConnectionString("DefaultConnection")
---
+++ AppCitas.Service/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppCitas.Service.Entities;
+
+namespace AppCitas.Service.Interfaces;
+
+public interface IUserRepository
+{
+    Task<AppUser> GetUserByIdAsync(int id);
+    Task<AppUser> GetUserByUsernameAsync(string username);
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+    Task<bool> SaveAllAsync();
+    void Update(AppUser user);
+}
