af81377bb8ef9f5af3fe8d3aae8f0a767b1538d1
Arturo Barajas <basaar.24@gmail.com>
2022-09-28 07:30:44 -0500

    Updating the users controller

--- AppCitas.Service/Controllers/UsersController.cs
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using AppCitas.Service.Data;
 using AppCitas.Service.Entities;
+using AppCitas.Service.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,22 +10,22 @@ namespace AppCitas.Service.Controllers;
 [Authorize]
 public class UsersController : BaseApiController
 {
-    private readonly DataContext _context;
+    private readonly IUserRepository _userRepository;
 
-    public UsersController(DataContext context)
+    public UsersController(IUserRepository userRepository)
     {
-        _context = context;
+        _userRepository = userRepository;
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        return await _context.Users.ToListAsync();
+        return Ok(await _userRepository.GetUsersAsync());
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    [HttpGet("{username}")]
+    public async Task<ActionResult<AppUser>> GetUserByUsername(string username)
     {
-        return await _context.Users.FindAsync(id);
+        return await _userRepository.GetUserByUsernameAsync(username);
     }
 }
