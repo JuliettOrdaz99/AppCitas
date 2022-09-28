5387871ffb854bd47d45cfb3a1d8f32f48bfd1d3
Arturo Barajas <basaar.24@gmail.com>
2022-09-28 07:53:41 -0500

    Adding AutoMapper

--- AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
+++ AppCitas.Service/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AppCitas.Service.Data;
+using AppCitas.Service.Helpers;
 using AppCitas.Service.Interfaces;
 using AppCitas.Service.Services;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ public static class ApplicationServiceExtensions
     {
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
         services.AddDbContext<DataContext>(options =>
         {
             options.UseSqlite(config.GetConnectionString("DefaultConnection")
---
+++ AppCitas.Service/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AppCitas.Service.DTOs;
+using AppCitas.Service.Entities;
+using AutoMapper;
+
+namespace AppCitas.Service.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+	public AutoMapperProfiles()
+	{
+		CreateMap<AppUser, MemberDto>();
+		CreateMap<Photo, PhotoDto>();
+	}
+}
