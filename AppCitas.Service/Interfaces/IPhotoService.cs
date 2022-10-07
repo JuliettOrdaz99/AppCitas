4a4c4a266987c2d739472ce4194fa0c162847bc1
Arturo Barajas <basaar.24@gmail.com>
2022-10-07 07:36:34 -0500

    Adding a photo service

---
+++ AppCitas.Service/Interfaces/IPhotoService.cs
@@ -0,0 +1,9 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace AppCitas.Service.Interfaces;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile photoFile);
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+}
---
+++ AppCitas.Service/Services/PhotoService.cs
@@ -0,0 +1,51 @@
+﻿using AppCitas.Service.Helpers;
+using AppCitas.Service.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+
+namespace AppCitas.Service.Services;
+
+public class PhotoService : IPhotoService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public PhotoService(IOptions<CloudinarySettings> config)
+    {
+        var acc = new Account
+        (
+            config.Value.CloudName,
+            config.Value.ApiKey,
+            config.Value.ApiSecret
+        );
+
+        _cloudinary = new Cloudinary(acc);
+    }
+
+    public async Task<ImageUploadResult> AddPhotoAsync(IFormFile photoFile)
+    {
+        var uploadResult = new ImageUploadResult();
+
+        if (photoFile.Length > 0)
+        {
+            using var stream = photoFile.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(photoFile.FileName, stream),
+                Transformation = new Transformation().Height(500).Width(500).Crop("Fill").Gravity("Face")
+            };
+            uploadResult = await _cloudinary.UploadAsync(uploadParams);
+        }
+
+        return uploadResult;
+    }
+
+    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+    {
+        var deleteParams = new DeletionParams(publicId);
+
+        var result = await _cloudinary.DestroyAsync(deleteParams);
+
+        return result;
+    }
+}
