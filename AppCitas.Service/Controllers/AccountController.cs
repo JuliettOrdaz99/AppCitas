28a599897e6b83ff28db03b089ba296537c9e9e2
Arturo Barajas <basaar.24@gmail.com>
2022-10-17 07:14:21 -0500

    Adding the main photo image to the nav bar

--- AppCitas.Service/Controllers/AccountController.cs
+++ AppCitas.Service/Controllers/AccountController.cs
@@ -49,6 +49,7 @@ public class AccountController : BaseApiController
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await _context.Users
+            .Include(p => p.Photos)
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
         if (user == null) return Unauthorized("Invalid username or password");
@@ -66,7 +67,8 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
     }
 
