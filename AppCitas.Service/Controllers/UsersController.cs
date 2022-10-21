dfc97bc2962af0e87302b0d9fcd77c5860e8da54
Arturo Barajas <basaar.24@gmail.com>
2022-10-21 07:32:46 -0500

    Adding filtering to the API

--- AppCitas.Service/Controllers/UsersController.cs
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -28,7 +28,13 @@ public class UsersController : BaseApiController
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
-    { 
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        userParams.CurrentUsername = user.UserName;
+
+        if (string.IsNullOrEmpty(userParams.Gender))
+            userParams.Gender = user.Gender == "male" ? "female" : "male";
+
         var users = await _userRepository.GetMembersAsync(userParams);
 
         Response.AddPaginationHeader(
