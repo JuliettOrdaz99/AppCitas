2e688c18b7f2730d7ae91d71a814421ed039e6af
Arturo Barajas <basaar.24@gmail.com>
2022-10-24 07:32:24 -0500

    Adding sorting functionality

--- AppCitas.Service/Helpers/UserParams.cs
+++ AppCitas.Service/Helpers/UserParams.cs
@@ -16,4 +16,5 @@ public class UserParams
     public string Gender { get; set; }
     public int MinAge { get; set; } = 18;
     public int MaxAge { get; set; } = 150;
+    public string OrderBy { get; set; } = "lastActive";
 }
