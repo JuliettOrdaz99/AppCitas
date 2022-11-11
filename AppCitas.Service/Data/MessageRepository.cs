c53d99c404a65c47adae3a1d505b7d80a84a391c
Arturo Barajas <basaar.24@gmail.com>
2022-11-11 07:28:13 -0600

    Getting the message thread for 2 users

--- AppCitas.Service/Data/MessageRepository.cs
+++ AppCitas.Service/Data/MessageRepository.cs
@@ -4,6 +4,7 @@ using AppCitas.Service.Helpers;
 using AppCitas.Service.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppCitas.Service.Data;
 
@@ -53,9 +54,33 @@ public class MessageRepository : IMessageRepository
             .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+    public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
     {
-        throw new NotImplementedException();
+        var messages = await _context.Messages
+            .Include(u => u.Sender).ThenInclude(p => p.Photos)
+            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            .Where(m => m.Recipient.UserName.Equals(currentUsername)
+                    && m.Sender.UserName.Equals(recipientUsername)
+                    || m.Recipient.UserName.Equals(recipientUsername)
+                    && m.Sender.UserName.Equals(currentUsername))
+            .OrderBy(m => m.MessageSent)
+            .ToListAsync();
+
+        var unreadMessages = messages
+            .Where(m => m.DateRead == null
+                    && m.Recipient.UserName.Equals(currentUsername)).ToList();
+
+        if (unreadMessages.Any())
+        {
+            foreach (var message in unreadMessages)
+            {
+                message.DateRead = DateTime.Now;
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        return _mapper.Map<IEnumerable<MessageDto>>(messages);
     }
 
     public async Task<bool> SaveAllAsync()
