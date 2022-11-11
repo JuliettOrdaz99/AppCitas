f01348cb19802cf7a0726cd56aef245ae062da45
Arturo Barajas <basaar.24@gmail.com>
2022-11-11 07:57:55 -0600

    Deleting messages on the client

--- AppCitas.Service/Data/MessageRepository.cs
+++ AppCitas.Service/Data/MessageRepository.cs
@@ -31,7 +31,10 @@ public class MessageRepository : IMessageRepository
 
     public async Task<Message> GetMessage(int id)
     {
-        return await _context.Messages.FindAsync(id);
+        return await _context.Messages
+            .Include(u => u.Sender)
+            .Include(u => u.Recipient)
+            .SingleOrDefaultAsync(x => x.Id == id);
     }
 
     public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
