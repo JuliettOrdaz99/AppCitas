601e33ae93c2a455ba762fc4e2a6020fc1f8db07
JuliettOrdaz99 <ajov_189420@hotmail.com>
2022-11-29 15:46:34 -0600

    listones

---
+++ AppCitas.UnitTests/Helpers/LoginHelper.cs
@@ -0,0 +1,53 @@
+﻿using AppCitas.Service.DTOs;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppCitas.UnitTests.Helpers
+{
+    public class LoginHelper
+    {
+        private static string registeredObject;
+        private static StringContent httpContent;
+
+        public static async Task<UserDto> LoginUser(string username, string password)
+        {
+            string requestUri = $"api/account/login";
+            HttpClient client = TestHelper.Instance.Client;
+            var loginDto = new LoginDto
+            {
+                Username = username,
+                Password = password
+            };
+            registeredObject = GetLoginObject(loginDto);
+            httpContent = GetHttpContent(registeredObject);
+            var result = await client.PostAsync(requestUri, httpContent);
+            var userJson = await result.Content.ReadAsStringAsync();
+            var user = JsonSerializer.Deserialize<UserDto>(userJson, new JsonSerializerOptions { 
+                PropertyNameCaseInsensitive = true
+            });
+            return user;
+        }
+        #region Privated methods
+        private static string GetLoginObject(LoginDto loginDto)
+        {
+            var entityObject = new JObject()
+            {
+                { nameof(loginDto.Username), loginDto.Username },
+                { nameof(loginDto.Password), loginDto.Password }
+            };
+            return entityObject.ToString();
+        }
+        public  static StringContent GetHttpContent(string objectToEncode)
+        {
+            return new StringContent(objectToEncode, Encoding.UTF8, "application/json");
+        }
+
+        #endregion
+    }
+}
---
+++ AppCitas.UnitTests/Tests/LikesControllerTests.cs
@@ -0,0 +1,155 @@
+﻿using AppCitas.Service.DTOs;
+using AppCitas.UnitTests.Helpers;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppCitas.UnitTests.Tests
+{
+    public class LikesControllerTests
+    {
+        private string apiRoute = "api/likes";
+        private readonly HttpClient _client;
+        private HttpResponseMessage httpResponse;
+        private string requestUri;
+        private string registeredObject;
+        private HttpContent httpContent;
+        public LikesControllerTests()
+        {
+            _client = TestHelper.Instance.Client;
+        }
+        
+        [Theory]
+        [InlineData("NotFound", "lisa", "Pa$$w0rd")]
+        public async Task Like_ShouldNotFound(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+
+            requestUri = $"{apiRoute}/julie";
+
+            // Act
+            httpResponse = await _client.PostAsync(requestUri, httpContent);
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("BadRequest", "lisa", "Pa$$w0rd")]
+        public async Task Like_ShouldBadRequest(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+
+            requestUri = $"{apiRoute}/lisa";
+
+            // Act
+            httpResponse = await _client.PostAsync(requestUri, httpContent);
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("OK", "lisa", "Pa$$w0rd")]
+        public async Task Like_ShouldOK(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+            
+            requestUri = $"{apiRoute}/todd";
+
+            // Act
+            httpResponse = await _client.PostAsync(requestUri, httpContent);
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("BadRequest", "lisa", "Pa$$w0rd")]
+        public async Task LikeRepeated_ShouldBadRequest(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+
+            requestUri = $"{apiRoute}/todd";
+
+            // Act
+            httpResponse = await _client.PostAsync(requestUri, httpContent);
+            httpResponse = await _client.PostAsync(requestUri, httpContent);
+
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("OK", "todd", "Pa$$w0rd")]
+        public async Task GetUserLikes_ShouldOK(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+
+            requestUri = $"{apiRoute}" + "?predicate=LIKEd";
+
+            // Act
+            httpResponse = await _client.GetAsync(requestUri);
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+        #region Privated methods
+        private static string GetRegisterObject(RegisterDto registerDto)
+        {
+            var entityObject = new JObject()
+            {
+                { nameof(registerDto.Username), registerDto.Username },
+                { nameof(registerDto.KnownAs), registerDto.KnownAs },
+                { nameof(registerDto.Gender), registerDto.Gender },
+                { nameof(registerDto.DateOfBirth), registerDto.DateOfBirth },
+                { nameof(registerDto.City), registerDto.City },
+                { nameof(registerDto.Country), registerDto.Country },
+                { nameof(registerDto.Password), registerDto.Password }
+            };
+
+            return entityObject.ToString();
+        }
+
+        private static string GetRegisterObject(LoginDto loginDto)
+        {
+            var entityObject = new JObject()
+            {
+                { nameof(loginDto.Username), loginDto.Username },
+                { nameof(loginDto.Password), loginDto.Password }
+            };
+            return entityObject.ToString();
+        }
+
+        private StringContent GetHttpContent(string objectToEncode)
+        {
+            return new StringContent(objectToEncode, Encoding.UTF8, "application/json");
+        }
+
+        #endregion
+    }
+}
